404e84e239a2702a6cf892a458126bce8923ef41
Leslie Bordonné <leslie@bordonne.com>
2024-08-24 16:57:12 -0600

    fruit grid setup

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,35 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function Square({ value , onSquareClick }) {
+  return (
+
+    <Flippy className="square"
+      flipOnHover={false} // default false
+      flipOnClick={true} // default false
+      flipDirection="horizontal" // horizontal or vertical
+      onClick={onSquareClick}
+    >
+      <FrontSide
+        style={{
+          backgroundImage: 'none',
+        }}
+      >
+      </FrontSide>
+      <BackSide
+        style={{ backgroundImage: 'url(/images/' + value + '.png)' }}>
+      </BackSide>
+    </Flippy>
+  );
+}
+
+export default function Board() {
+  let images = Array(12).fill(null);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  setImages();
+  function setImages() {
+    let imgArray = [1,2,3,4,5,6,7,8,9];
+
+    let deck = [];
+    // takes 6 random numbers from 1 to 9
+
+    for (let i=0; i<6; i++) {
+      let g = Math.floor(Math.random() * imgArray.length);
+      deck[i] = imgArray[g];
+      // delete the number from the list
+      imgArray.splice(g,1);
+    }
+
+    console.log("pick of images");
+    console.log(deck);
+
+    console.log("make pairs");
+    images = deck.concat(deck);
+    console.log(images);
+
+    console.log("shuffle array");
+    images = shuffleArray(images);
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board-row">
+        <Square value={images[0]} onSquareClick={() => console.log("test")} />
+        <Square value={images[1]} />
+        <Square value={images[2]} />
+        <Square value={images[3]} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <Square value={images[4]} />
+        <Square value={images[5]} />
+        <Square value={images[6]} />
+        <Square value={images[7]} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="board-row">
+        <Square value={images[8]} />
+        <Square value={images[9]} />
+        <Square value={images[10]} />
+        <Square value={images[11]} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
+}
